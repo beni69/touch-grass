7a89f6513b5a2108e756bc49202be254d34170bd
beni69 <73666440+beni69@users.noreply.github.com>
2022-11-23 18:22:40 +0100

    4th commit

--- .github/workflows/grass.sh
+++ .github/workflows/grass.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-GRASS_LIMIT=3
+GRASS_LIMIT=4
 
 COUNT=$(git log --after=yesterday --pretty=oneline | wc -l)
 
-if [ COUNT -gt GRASS_LIMIT ]; then
+if [ $COUNT -gt $GRASS_LIMIT ]; then
     echo "TOUCH GRASS LMAO"
+else
+    echo "fine for now"
 fi
